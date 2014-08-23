1fcc950328101e91babbbeeefcd63a18814743ba
johnfn <johnfn@gmail.com>
2014-08-23 13:50:33 -0700

    Finally seeing some graphics onscreen.
    That took entirely too long <_<

--- src/lasers.ts
+++ src/lasers.ts
@@ -1,9 +1,14 @@
 /// <reference path="references.ts" />
 
 class Laser extends Phaser.Sprite {
+  laserLine:Phaser.Line;
+  data:Phaser.BitmapData;
+
   constructor(a, b, c, d, e) {
     super(a, b, c, d, e);
 
-    console.log("lazer!");
+    var shape = G.game.add.graphics(0, 0);
+    shape.beginFill(0xff0000, 1);
+    shape.drawRect(0, 0, 20, 500);
   }
 }
