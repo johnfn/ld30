5add425cfc04e931b9bad8ff2caeab2ba74e7c84
johnfn <johnfn@gmail.com>
2014-08-23 12:34:45 -0700

    Press x.

--- src/dialog.ts
+++ src/dialog.ts
@@ -4,12 +4,15 @@ class Dialog extends Phaser.Group {
   bottomOffset:number = 50;
   box:Phaser.Sprite;
   text:Phaser.Text;
+  pressXText:Phaser.Text;
+  style:any;
 
   constructor() {
     super(G.game);
 
     this.addBox();
     this.addText();
+    this.addPressXText();
 
     this.fixedToCamera = true;
   }
@@ -21,18 +24,29 @@ class Dialog extends Phaser.Group {
     this.box.x = (G.SCREEN_WIDTH - this.box.width) / 2;
 
     this.add(this.box);
+
+    this.setStyle();
   }
 
-  addText() {
-    var style = {
+  setStyle() {
+    this.style = {
       wordWrapWidth: this.box.width - 20,
       font: "15px Arial",
       wordWrap: true,
-      fill: "#000000"
+      fill: "#000"
     };
+  }
 
-    this.text = new Phaser.Text(G.game, this.box.x + 10, this.box.y + 10, "Werdz werds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds werds", style);
-
+  addText() {
+    this.text = new Phaser.Text(G.game, this.box.x + 10, this.box.y + 10, "Werdz werds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds wwerds werds werds", this.style);
     this.add(this.text);
+  }
+
+  addPressXText() {
+    this.pressXText = new Phaser.Text(G.game, 0, 0, "Press X to continue", this.style);
+
+    this.pressXText.x = this.box.x + this.box.width - this.pressXText.width - 10;
+    this.pressXText.y = this.box.y + this.box.height - this.pressXText.height;
+    this.add(this.pressXText);
   }
 }
